55460b36f851347223415cd50616075d51839877
abdurahmonjonov <abdullajonabdurahmonjonov@gmail.com>
2026-08-09 11:30:52 +0500

    Add Pulsar app icon

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,18 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         start_url: '/',
-        icons: []
+        icons: [
+          {
+            src: '/icons/pulsar-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/pulsar-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ]
